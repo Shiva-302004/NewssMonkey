16a39cd924648c1dba6f32491b3cf96be9a2aac3
Shiva-302004 <121721793+Shiva-302004@users.noreply.github.com>
2023-06-11 11:37:15 +0530

    sticky nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,9 @@
 import React from 'react'
 
 const Navbar = (props) => {
-
-
-
-    return (
+     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-dark">
+            <nav className="navbar fixed-top navbar-expand-lg bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand text-warning " href="/">NewMonkey</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
